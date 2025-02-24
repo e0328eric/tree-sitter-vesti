58ad64b044909a76910ef37b956b7c8200f2d837
Sungbae Jeong <almagest0328@gmail.com>
2025-02-24 12:21:26 +0900

    change version into 15 -> 14

---
+++ bindings/c/tree-sitter-vesti.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_VESTI_H_
+#define TREE_SITTER_VESTI_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_vesti(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_VESTI_H_
