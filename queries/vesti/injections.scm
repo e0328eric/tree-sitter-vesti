a627fdc829f8a4ab3c77252f57bd3da75a49db31
Sungbae Jeong <almagest0328@gmail.com>
2025-09-20 14:51:51 +0900

    fooo

--- queries/vesti/injections.scm
+++ queries/vesti/injections.scm
@@ -1,6 +1,3 @@
-; Combine all line contents into a single Python virtual buffer
 (jlcode_block
-  (jlcode_line
-    (jlcode_line_content) @injection.content)
+  (jlcode_line_content) @injection.content
   (#set! injection.language "julia"))
-
