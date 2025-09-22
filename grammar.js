163cc0b8e64c771a518d06f89647579679272056
Sungbae Jeong <almagest0328@gmail.com>
2025-09-22 15:04:10 +0900

    asdasd

--- grammar.js
+++ grammar.js
@@ -125,10 +125,7 @@ module.exports = grammar({
       ),
 
     jlcode_block: ($) =>
-      prec.right(
-        1,
         seq($.KEYWORD_jlcode, $.jlcode_line_content, $.KEYWORD_endjlcode),
-      ),
 
     KEYWORD_docclass: ($) => token("docclass"),
     KEYWORD_importpkg: ($) => token("importpkg"),
