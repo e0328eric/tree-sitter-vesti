4a73a1678875a6d9cb975ebdb59bae4651064274
Sungbae Jeong <almagest0328@gmail.com>
2026-02-15 21:53:21 +0900

    pqikadw

--- grammar.js
+++ grammar.js
@@ -170,6 +170,6 @@ module.exports = grammar({
     multiline_raw_latex: (_) => token(seq("%-", /[^-]*-+([^%-][^-]*-+)*/, "%")), // Comments // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     line_comment: (_) => token(seq("--", /[^\n]*/, /\r?\n/)),
     long_comment: ($) =>
-      seq($.long_comment_start, $.long_comment_content, $.long_comment_end),
+      seq($.long_comment_start, $.long_comment_payload, $.long_comment_end),
   },
 });
